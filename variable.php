81f7e2daa5665576194a4703cea1fb54bcb52dd5
Md Rahat Islam <islammdrahat308@gmail.com>
2024-09-20 11:39:14 +0600

    20/09/24

--- variable.php
+++ variable.php
@@ -19,6 +19,66 @@ var_dump([2, 3, 56]);
 var_dump(NULL);
 
 ?>
+
+<!-- Three types of Variable are in PHP -->
+ <!-- 1, Local 
+ 2, global
+ 3, Static -->
+
+
+ <!-- Local Variable -->
+  
+ <?php
+//  $local=5;
+
+ function lVariable(){
+    $local = 5;
+    echo "</br> $local";
+ }
+ 
+ lVariable();
+ 
+ ?>
+
+ <!-- Global Variable  -->
+ <?php
+ $gVariable=10;
+ $ggVariable= 5;
+
+
+ function gAble(){
+
+    global $gVariable , $ggVariable;
+    echo $gVariable + $ggVariable;
+
+ }
+ 
+ gAble();
+ 
+echo" </br> $gVariable";
+
+ ?>
+ 
+ <?php
+
+ echo "<br>";
+ //This is static variable
+function sVariable(){
+  static $iStatic = 5;
+    echo $iStatic;
+    $iStatic++;
+}
+
+
+sVariable();
+echo "<br>";
+sVariable();
+echo "<br>";
+sVariable();
+echo "<br>";
+?>
+
+
 </body>
 </html>
 
