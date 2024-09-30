896e583f20c9343e24a009c90d4aa8313837a98f
Md Rahat Islam <islammdrahat308@gmail.com>
2024-09-30 22:53:46 +0600

    30/09/24

---
+++ function.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+//Default argument is comming
+function setHeight($minheight = 50) {
+  echo "The height is : $minheight <br>";
+}
+
+setHeight(350);
+setHeight();
+setHeight(135);
+setHeight(80);
+
+
+
+
+function sum($x, $y) {
+    $z = $x + $y;
+    return $z;
+  }
+  
+  echo "5 + 10 = " . sum(5,10) . "<br>";
+  echo "7 + 13 = " . sum(7,13) . "<br>";
+  echo "2 + 4 = " . sum(2,4);
+
+
+echo "<br>";
+
+
+  function add_five(&$value) {
+    $value += 5;
+  }
+  
+  $num = 5;
+  add_five($num);
+  echo $num;
+
+
+  function sumMyNumbers(...$x) {
+    $n = 0;
+    $len = count($x);
+    for($i = 0; $i < $len; $i++) {
+      $n += $x[$i];
+    }
+    return $n;
+  }
+  
+  $a = sumMyNumbers(5, 2, 6, 2, 7, 7);
+  echo $a;
+
+  echo "<br>";
+
+  $x = 75;
+  
+  function myfunction() {
+    global $x;
+    echo $x;
+  }
+  
+  myfunction();
+
+echo "<br>";
+
+$x = 100;
+
+echo $GLOBALS["x"];
+echo "<br>". $x;
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+
+
+
+?>
+</body>
+</html>
